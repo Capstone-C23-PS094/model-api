7f5c9eec6cea70764c105c7c77a1c49a2bc06f71
Iman <imannurohman1@gmail.com>
2023-06-13 15:17:20 +0700

    add data for result

---
+++ data.py
@@ -0,0 +1,90 @@
+crop_data = {
+    'rice': {
+        'name': 'Padi',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641361/rice_czmu1m.jpg',
+    },
+    'maize': {
+        'name': 'Jagung',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641356/maize_cxkflw.jpg',
+    },
+    'chickpea': {
+        'name': 'Kacang Arab',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641343/chickpea_oo3la0.jpg',
+    },
+    'kidneybeans': {
+        'name': 'Kacang Merah',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641357/kidneybeans_xaq2fs.jpg',
+    },
+    'pigeonpeas': {
+        'name': 'Kacang Polong',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641360/pigeonpeas_giixak.jpg',
+    },
+    'mothbeans': {
+        'name': 'Kacang Kedelai',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686642226/mothbeans_ehp9rc.jpg',
+    },
+    'mungbean': {
+        'name': 'Kacang Hijau',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641358/mungbean_f98ir0.jpg',
+    },
+    'blackgram': {
+        'name': 'Kacang Hitam',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641348/blackgram_rarcgd.jpg',
+    },
+    'lentil': {
+        'name': 'Kacang Lentil',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641349/lentil_vijcp0.jpg',
+    },
+    'pomegranate': {
+        'name': 'Delima',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641363/pomegranate_lzhqoy.jpg',
+    },
+    'banana': {
+        'name': 'Pisang',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641342/banana_ujcnz3.jpg',
+    },
+    'mango': {
+        'name': 'Mangga',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641358/mango_ciitdt.jpg',
+    },
+    'grapes': {
+        'name': 'Anggur',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641349/grapes_olkarq.jpg',
+    },
+    'watermelon': {
+        'name': 'Semangka',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641361/watermelon_obm8o2.jpg',
+    },
+    'muskmelon': {
+        'name': 'Melon',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641367/muskmelon_dbkaiz.jpg',
+    },
+    'apple': {
+        'name': 'Apel',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641343/apple_bkfykg.jpg',
+    },
+    'orange': {
+        'name': 'Jeruk',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641359/orange_n84s7v.jpg',
+    },
+    'papaya': {
+        'name': 'Pepaya',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641359/papaya_aks62e.jpg',
+    },
+    'coconut': {
+        'name': 'Kelapa',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641343/coconut_ab40u2.jpg',
+    },
+    'cotton': {
+        'name': 'Kapas',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641343/cotton_afphkl.jpg',
+    },
+    'jute': {
+        'name': 'Ijuk',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641365/jute_q3cf0x.jpg',
+    },
+    'coffee': {
+        'name': 'Kopi',
+        'url': 'https://res.cloudinary.com/daktccmqn/image/upload/v1686641343/coffee_y3s71a.jpg',
+    }
+}
